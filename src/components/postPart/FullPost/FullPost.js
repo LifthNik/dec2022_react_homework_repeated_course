46596d5c16ab4abca5daf919ba8ac65208edf1bb
LifthNik <hikki20088@gmail.com>
2023-04-14 14:47:52 +0300

    HW2

--- src/components/postPart/FullPost/FullPost.js
+++ src/components/postPart/FullPost/FullPost.js
@@ -3,14 +3,21 @@ import React, { useState, useEffect } from "react";
 import "./FullPost.css";
 
 const FullPost = ({ value }) => {
+
     const [opacity, setOpacity] = useState(0);
 
     useEffect(() => {
         setOpacity(1);
+
+        const timeout = setTimeout(() => {
+            setOpacity(0);
+        }, 3000);
+
+        return () => clearTimeout(timeout);
     }, [value]);
 
     return (
-        <div className="FullPost" style={{ opacity }}>
+        <div className="FullPost" style={{opacity}}>
             <h2>{value.title}</h2>
             <p>{value.body}</p>
             <div className="meta-info">
