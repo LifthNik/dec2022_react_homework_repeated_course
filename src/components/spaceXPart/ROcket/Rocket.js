bc31854554305601662bc25afb8843ad126f1538
LifthNik <hikki20088@gmail.com>
2023-04-14 15:33:35 +0300

    HW2

---
+++ src/components/spaceXPart/ROcket/Rocket.js
@@ -0,0 +1,14 @@
+import './Rocket.css'
+
+
+export default function Rocket({ item }) {
+    return (
+        <div className="Rocket">
+            <div className="Rocket-details">
+                <h3>{item.mission_name}</h3>
+                <p>Year: {item.launch_year}</p>
+            </div>
+            <img src={item.links?.mission_patch_small} alt={item.mission_name} />
+        </div>
+    );
+};
--- src/components/spaceXPart/ROckets/Rockets.js
+++ src/components/spaceXPart/ROckets/Rockets.js
@@ -1,7 +1,25 @@
+import {useEffect, useState} from "react";
+
+import Rocket from "../ROcket/Rocket";
+
+import './Rockets.css'
+
 export default function Rockets () {
 
+    let [rockets, setRockets] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://api.spacexdata.com/v3/launches/`)
+            .then((items) => items.json())
+            .then((item) => {
+                setRockets(item.filter(year => year.launch_year !== '2020'))
+            });
+    }, []);
+
     return (<div className = 'Rockets'>
-                Rockets
+            {
+                rockets.map(item => <Rocket item={item} key={item.flight_number}/>)
+            }
         </div>
     );
 };
